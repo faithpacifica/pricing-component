efa6240cc7dd7a883490fbad9fc0488a49d81b63
Dildora Muslimova <85798640+faithpacifica@users.noreply.github.com>
2022-01-04 09:36:26 +0500

    js code add

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
 
-const elToggleButton = document.querySelector('.header__switcher--checkbox');
-const cardPrice = document.querySelector('.card__price--amount');
+// const elToggleButton = document.querySelector('.header__switcher--checkbox');
+// const cardPrice = document.querySelector('.card__price--amount');
 
 
-elToggleButton.addEventListener('click',function() {
-    cardPrice.classList.add(' hidden') 
-    cardPrice.classList.remove(' hidden') 
+// elToggleButton.addEventListener('click',function() {
+//     cardPrice.classList.add(' hidden') 
+//     cardPrice.classList.remove(' hidden') 
+// }
+// )
+   
+
+
+    function myFunction() {
+        const checkBox = document.getElementById("toggle");
+        console.log(checkBox)
+
+        const priceEl = document.querySelectorAll(".hidden");
+        console.log(priceEl)
+
+        const optionEl = document.querySelectorAll(".header__toggle--option");
+        console.log(optionEl)
+
+        
+       if (checkBox.checked){
+        priceEl.style.display = 'block'
+        optionEl.classList.add(' active') 
+    } else {
+        priceEl.style.display = 'none'
+        optionEl.classList.remove(' active') 
+  }
 }
-)
+
