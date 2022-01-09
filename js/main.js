4d52e4ec9236528abea2d9d1e7243930fe223af0
Dildora Muslimova <85798640+faithpacifica@users.noreply.github.com>
2022-01-10 02:06:26 +0500

    toggle-button dynamically function

--- js/main.js
+++ js/main.js
@@ -1,53 +1,22 @@
 
-// const elToggleButton = document.querySelector('.header__switcher--checkbox');
-// const cardPrice = document.querySelector('.card__price--amount');
+const elPaymentRadio = document.querySelectorAll('[name="payment-period"]');
+const elPriceValue = document.querySelectorAll('.card__price-value');
 
+elPaymentRadio.forEach(function(el) {
+    el.addEventListener('change', function () {
 
-// elToggleButton.addEventListener('click',function() {
-//     cardPrice.classList.add(' hidden') 
-//     cardPrice.classList.remove(' hidden') 
-// }
-// )
-   
+        const period = el.value;
 
+        if (period === 'annual' ) {
+            elPriceValue.forEach(function (elPrice) {
+                elPrice.textContent = elPrice.closest('.card').dataset.annual;
+            });
+        } else {
+            elPriceValue.forEach(function (elPrice) {
+                elPrice.textContent = elPrice.closest('.card').dataset.monthly;
+            })
+        }
+    })
+})
 
-    function myFunction() {
-        const checkBox = document.getElementById("toggle");
-        console.log(checkBox)
-
-        const priceEl = document.querySelectorAll(".hidden");
-        console.log(priceEl)
-
-        const optionEl = document.querySelector(".header__toggle--option");
-        console.log(optionEl)
-
-        const ElPriceOrigin = document.querySelectorAll(".price-origin");
-        console.log(ElPriceOrigin)
-
-        
-       if (checkBox.checked){
-        priceEl.forEach(el =>
-            el.style.display = 'block' 
-        )
-
-        ElPriceOrigin.forEach(el =>
-            el.style.display = 'none' 
-        )
-
-        optionEl.classList.add('active') 
-    } 
-    
-    else {
-
-        priceEl.forEach(el =>
-            el.style.display = 'none' 
-        )
-
-        ElPriceOrigin.forEach(el =>
-            el.style.display = 'block' 
-        )
-
-        optionEl.classList.remove(' active') 
-  }
-}
 
